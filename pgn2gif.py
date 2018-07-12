0e0d83c8706a2ee31eff3b79bb46edaf2691dad0
Deniz Kızılırmak <kizilirmakmd@gmail.com>
2018-07-12 17:21:24 +0530

    Fix PEP8

--- pgn2gif.py
+++ pgn2gif.py
@@ -97,15 +97,15 @@ def update(move: str, turn: int):
             row = '8'
         kl = 'e' + row
         if move.count('O') == 2:
-            ks = 'g'+row
-            rs = 'f'+row
-            clear(pixels_from_square('h'+row))
-            update_pieces('h'+row, rs, r)
+            ks = 'g' + row
+            rs = 'f' + row
+            clear(pixels_from_square('h' + row))
+            update_pieces('h' + row, rs, r)
         else:
             ks = 'c' + row
             rs = 'd' + row
-            clear(pixels_from_square('a'+row))
-            update_pieces('a'+row, rs, r)
+            clear(pixels_from_square('a' + row))
+            update_pieces('a' + row, rs, r)
 
         update_pieces(kl, ks, k)
         clear(pixels_from_square(kl))
@@ -130,9 +130,9 @@ def check_line(sqr1: str, sqr2: str) -> bool:
     if r1 == r2:
         i1 = columns.index(c1)
         i2 = columns.index(c2)
-        return all(pieces[columns[i]+str(r1)] == '' for i in range(min(i1, i2)+1, max(i1, i2)))
+        return all(pieces[columns[i] + str(r1)] == '' for i in range(min(i1, i2) + 1, max(i1, i2)))
     elif c1 == c2:
-        return all(pieces[c1+str(i)] == '' for i in range(min(r1, r2)+1, max(r1, r2)))
+        return all(pieces[c1 + str(i)] == '' for i in range(min(r1, r2) + 1, max(r1, r2)))
     return False
 
 
@@ -148,9 +148,9 @@ def check_diagonal(sqr1: str, sqr2: str) -> bool:
             return all(pieces[columns[min_c+i] + str(min_r + i)] == '' for i in range(1, abs(c1-c2)))
 
         elif c1 > c2 and r2 > r1:
-            return all(pieces[columns[c2+i]+str(r2-i)] == '' for i in range(1, c1-c2))
+            return all(pieces[columns[c2 + i] + str(r2 - i)] == '' for i in range(1, c1 - c2))
 
-        return all(pieces[columns[c2-i] + str(r2+i)] == '' for i in range(1, c2-c1))
+        return all(pieces[columns[c2 - i] + str(r2 + i)] == '' for i in range(1, c2 - c1))
     return False
 
 
@@ -179,16 +179,16 @@ def source_of_move(move: str, turn: int) -> tuple:
         # Pushing pawn e.g. e4
         if len(move) == 2:
             if turn == 0:
-                source = c + next(str(i) for i in range(r, 0, -1) if pieces[c+str(i)] == p)
+                source = c + next(str(i) for i in range(r, 0, -1) if pieces[c + str(i)] == p)
             else:
-                source = c + next(str(i) for i in range(r, 9) if pieces[c+str(i)] == p)
+                source = c + next(str(i) for i in range(r, 9) if pieces[c + str(i)] == p)
         # Capturing pawn e.g. ed5
         elif len(move) == 3:
             if pieces[move[-2:]] != '':
                 if turn == 0:
-                    source = move[0] + str(r-1)
+                    source = move[0] + str(r - 1)
                 else:
-                    source = move[0] + str(r+1)
+                    source = move[0] + str(r + 1)
             # En Passant
             else:
                 if turn == 0:
@@ -216,17 +216,17 @@ def source_of_move(move: str, turn: int) -> tuple:
         # e.g. Ra3
         if len(move) == 3:
             try:
-                source = c + next(row for row in rows if pieces[c + str(row)] == p and check_line(c+str(row), to))
+                source = c + next(row for row in rows if pieces[c + str(row)] == p and check_line(c + str(row), to))
+
             except:
-                source = next(col for col in columns if pieces[col + str(r)]
-                              == p and check_line(col+str(r), to)) + str(r)
+                source = next(col for col in columns if pieces[col + str(r)] == p and check_line(col + str(r), to)) + str(r)
         # e.g. Raa3
         else:
             indicator = move[1]
             if indicator in columns:
-                source = indicator + next(row for row in rows if pieces[indicator+row] == p)
+                source = indicator + next(row for row in rows if pieces[indicator + row] == p)
             else:
-                source = next(col for col in columns if pieces[col+indicator] == p) + indicator
+                source = next(col for col in columns if pieces[col + indicator] == p) + indicator
 
     elif move[0] == 'Q':
         p = 'wq' if turn == 0 else 'bq'
