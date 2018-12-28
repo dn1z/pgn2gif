384d5eac0ac8cf051ce36ce6a8079cdb04460b4b
Deniz Kızılırmak <kizilirmakmd@gmail.com>
2018-12-28 20:18:56 +0300

    Added ability to specify colors for white and black squares

--- pgn2gif.py
+++ pgn2gif.py
@@ -10,9 +10,6 @@ from PIL import Image as img
 columns = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
 rows = ['1', '2', '3', '4', '5', '6', '7', '8']
 
-black_square = img.new('RGBA', (60, 60), (75, 115, 153))
-white_square = img.new('RGBA', (60, 60), (234, 233, 210))
-
 bk = img.open('./images/bk.png')
 bq = img.open('./images/bq.png')
 bb = img.open('./images/bb.png')
@@ -38,7 +35,7 @@ def get_moves_from_pgn(file_path):
 def coordinates_of_square(square):
     c = columns.index(square[0])
     r = 7 - rows.index(square[1])
-    if is_reversed:
+    if reverse:
         return ((7 - c) * 60, (7 - r) * 60)
     else:
         return (c * 60, r * 60)
@@ -214,16 +211,7 @@ def promotion(move, is_white_turn):
     exec('board_image.paste({0},to,{0})'.format(piece_type))
 
 
-def create_gif(file_name, out_name, duration, output_dir, reverse):
-    global is_reversed
-    is_reversed = reverse
-
-    global board_image
-    if is_reversed:
-        board_image = img.open('./images/board2.png')
-    else:
-        board_image = img.open('./images/board.png')
-
+def create_gif(file_name, out_name, duration, output_dir):
     global board
     board =  {'a8': 'br', 'b8': 'bn', 'c8': 'bb', 'd8': 'bq', 'e8': 'bk', 'f8': 'bb', 'g8': 'bn', 'h8': 'br',
               'a7': 'bp', 'b7': 'bp', 'c7': 'bp', 'd7': 'bp', 'e7': 'bp', 'f7': 'bp', 'g7': 'bp', 'h7': 'bp',
@@ -247,15 +235,55 @@ def create_gif(file_name, out_name, duration, output_dir, reverse):
     imageio.mimsave(output_dir + '/' + out_name, images, duration=duration)
 
 
-def process_file(pgn, duration, output_dir, reverse):
+def process_file(pgn, duration, output_dir):
     basename = os.path.basename(pgn)
     name = basename[:-4] + '.gif'
     if name in os.listdir('.'):
         print("gif with name %s already exists." % name)
     else:
         print('Creating ' + name, end='.... ')
-        create_gif(pgn, name, duration, output_dir, reverse)
+        create_gif(pgn, name, duration, output_dir)
         print('done')
+
+
+def generate_board():
+    global board_image
+    board_image = img.new('RGB', (480, 480))
+
+    for i in range(0, 480, 60):
+        for j in range(0, 480, 60):
+            clear((i, j))
+
+    for i in range(0, 8):
+        if reverse:
+            board_image.paste(wp, (60 * i, 60), wp)
+            board_image.paste(bp, (60 * i, 360), bp)
+        else:
+            board_image.paste(bp, (60 * i, 60), bp)
+            board_image.paste(wp, (60 * i, 360), wp)
+
+    for i,p in enumerate(['r', 'n', 'b']):
+        if reverse:
+            exec("board_image.paste(w{0}, ({1}, 0), w{0})".format(p, i * 60))
+            exec("board_image.paste(b{0}, ({1}, 420), b{0})".format(p, 420 - i * 60))
+            exec("board_image.paste(b{0}, ({1}, 420), b{0})".format(p, i * 60))
+            exec("board_image.paste(w{0}, ({1}, 0), w{0})".format(p, 420 - i * 60))
+        else:
+            exec("board_image.paste(b{0}, ({1}, 0), b{0})".format(p, i * 60))
+            exec("board_image.paste(w{0}, ({1}, 420), w{0})".format(p, 420 - i * 60))
+            exec("board_image.paste(w{0}, ({1}, 420), w{0})".format(p, i * 60))
+            exec("board_image.paste(b{0}, ({1}, 0), b{0})".format(p, 420 - i * 60))
+
+    if reverse:
+        board_image.paste(wk, (180, 0), wk)
+        board_image.paste(wq, (240, 0), wq)
+        board_image.paste(bk, (180, 420), bk)
+        board_image.paste(bq, (240, 420), bq)
+    else:
+        board_image.paste(wk, (240, 420), wk)
+        board_image.paste(wq, (180, 420), wq)
+        board_image.paste(bk, (240, 0), bk)
+        board_image.paste(bq, (180, 0), bq)
 
 
 def main():
@@ -264,14 +292,29 @@ def main():
     parser.add_argument('-d', '--delay', help='Delay between moves in seconds', default=0.4)
     parser.add_argument('-o', '--out', help='Name of the output folder', default=os.getcwd())
     parser.add_argument('-r', '--reverse', help='Reverse board', action='store_true')
+    parser.add_argument('--black-square-color', help='Color of black squares in hex', default='#4B7399')
+    parser.add_argument('--white-square-color', help='Color of white squares in hex', default='#EAE9D2')
     args = parser.parse_args()
 
+    to_rgb = lambda h:tuple(int(h[i:i+2], 16) for i in (0, 2 ,4))
+
+    global reverse
+    reverse = args.reverse
+
+    global white_square
+    white_square = img.new('RGBA', (60, 60), to_rgb(args.white_square_color.lstrip('#')))
+
+    global black_square
+    black_square = img.new('RGBA', (60, 60), to_rgb(args.black_square_color.lstrip('#')))
+
+    generate_board()
+
     if os.path.isfile(args.path):
-        process_file(args.path, args.delay, args.out, args.reverse)
+        process_file(args.path, args.delay, args.out)
 
     elif os.path.isdir(args.path):
         for pgn in glob.glob(args.path + '*.pgn'):
-            process_file(pgn, args.delay, args.out, args.reverse)
+            process_file(pgn, args.delay, args.out)
 
 
 if __name__ == '__main__':
