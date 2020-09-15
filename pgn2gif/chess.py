340f4ab8d659222b4f932738e23637a023900973
Deniz Kızılırmak <kizilirmakmd@gmail.com>
2020-09-15 18:10:53 +0300

    Rewrited some parts to improve performance

--- pgn2gif/chess.py
+++ pgn2gif/chess.py
@@ -1,77 +1,84 @@
 import re
 
+INITIAL_STATE = {
+    'a8': 'br',
+    'b8': 'bn',
+    'c8': 'bb',
+    'd8': 'bq',
+    'e8': 'bk',
+    'f8': 'bb',
+    'g8': 'bn',
+    'h8': 'br',
+    'a7': 'bp',
+    'b7': 'bp',
+    'c7': 'bp',
+    'd7': 'bp',
+    'e7': 'bp',
+    'f7': 'bp',
+    'g7': 'bp',
+    'h7': 'bp',
+    'a6': '',
+    'b6': '',
+    'c6': '',
+    'd6': '',
+    'e6': '',
+    'f6': '',
+    'g6': '',
+    'h6': '',
+    'a5': '',
+    'b5': '',
+    'c5': '',
+    'd5': '',
+    'e5': '',
+    'f5': '',
+    'g5': '',
+    'h5': '',
+    'a4': '',
+    'b4': '',
+    'c4': '',
+    'd4': '',
+    'e4': '',
+    'f4': '',
+    'g4': '',
+    'h4': '',
+    'a3': '',
+    'b3': '',
+    'c3': '',
+    'd3': '',
+    'e3': '',
+    'f3': '',
+    'g3': '',
+    'h3': '',
+    'a2': 'wp',
+    'b2': 'wp',
+    'c2': 'wp',
+    'd2': 'wp',
+    'e2': 'wp',
+    'f2': 'wp',
+    'g2': 'wp',
+    'h2': 'wp',
+    'a1': 'wr',
+    'b1': 'wn',
+    'c1': 'wb',
+    'd1': 'wq',
+    'e1': 'wk',
+    'f1': 'wb',
+    'g1': 'wn',
+    'h1': 'wr'
+}
+
 COLUMNS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
 
 
-class ChessGame(object):
-    def __init__(self):
+class ChessGame:
+    def __init__(self, pgn):
+        self.is_finished = False
         self.is_white_turn = True
-        self.state = {
-            'a8': 'br',
-            'b8': 'bn',
-            'c8': 'bb',
-            'd8': 'bq',
-            'e8': 'bk',
-            'f8': 'bb',
-            'g8': 'bn',
-            'h8': 'br',
-            'a7': 'bp',
-            'b7': 'bp',
-            'c7': 'bp',
-            'd7': 'bp',
-            'e7': 'bp',
-            'f7': 'bp',
-            'g7': 'bp',
-            'h7': 'bp',
-            'a6': '',
-            'b6': '',
-            'c6': '',
-            'd6': '',
-            'e6': '',
-            'f6': '',
-            'g6': '',
-            'h6': '',
-            'a5': '',
-            'b5': '',
-            'c5': '',
-            'd5': '',
-            'e5': '',
-            'f5': '',
-            'g5': '',
-            'h5': '',
-            'a4': '',
-            'b4': '',
-            'c4': '',
-            'd4': '',
-            'e4': '',
-            'f4': '',
-            'g4': '',
-            'h4': '',
-            'a3': '',
-            'b3': '',
-            'c3': '',
-            'd3': '',
-            'e3': '',
-            'f3': '',
-            'g3': '',
-            'h3': '',
-            'a2': 'wp',
-            'b2': 'wp',
-            'c2': 'wp',
-            'd2': 'wp',
-            'e2': 'wp',
-            'f2': 'wp',
-            'g2': 'wp',
-            'h2': 'wp',
-            'a1': 'wr',
-            'b1': 'wn',
-            'c1': 'wb',
-            'd1': 'wq',
-            'e1': 'wk',
-            'f1': 'wb',
-            'g1': 'wn',
-            'h1': 'wr'
-        }
+        self.__last_played_move_index = -1
+
+        self.moves = self.__parse_pgn_file(pgn)
+
+        self.state = INITIAL_STATE.copy()
 
     def __check_knight_move(self, sqr1, sqr2):
         cd = abs(ord(sqr1[0]) - ord(sqr2[0]))
@@ -186,7 +193,26 @@ class ChessGame(object):
         origin = move[0] + ('7' if self.is_white_turn else '2')
         self.__update_state(origin, move[-4:-2], pt)
 
-    def push(self, move):
+    def __parse_pgn_file(self, pgn):
+        with open(pgn) as p:
+            data = p.read()
+            data = re.sub(r'\{.*?\}', '', data)  # Removes pgn comments
+            moves = re.findall(
+                r'[a-h]x?[a-h]?[1-8]=?[BKNRQ]?|O-O-?O?|[BKNRQ][a-h1-8]?[a-h1-8]?x?[a-h][1-8]',
+                data)
+            return [move.replace('x', '') for move in moves]
+
+    def next(self):
+        if self.is_finished:
+            return
+
+        self.__last_played_move_index += 1
+        try:
+            move = self.moves[self.__last_played_move_index]
+        except:
+            self.is_finished = True
+            return
+
         if 'O' in move:
             self.__castle(move)
 
@@ -205,13 +231,3 @@ class ChessGame(object):
             self.__update_state(origin, dest, pt)
 
         self.is_white_turn = not self.is_white_turn
-
-
-def get_moves_from_pgn(pgn):
-    with open(pgn) as p:
-        data = p.read()
-        data = re.sub(r'\{.*?\}', '', data)  # Removes pgn comments
-        moves = re.findall(
-            r'[a-h]x?[a-h]?[1-8]=?[BKNRQ]?|O-O-?O?|[BKNRQ][a-h1-8]?[a-h1-8]?x?[a-h][1-8]',
-            data)
-        return [move.replace('x', '') for move in moves]
--- pgn2gif/pgn2gif.py
+++ pgn2gif/pgn2gif.py
@@ -4,36 +4,15 @@ except ImportError:
     import chess
 
 import os
-import glob
 import argparse
 
 import imageio
 from PIL import Image
 from numpy import array
 
-cwd = os.getcwd()
-script_dir = os.path.dirname(os.path.abspath(__file__))
-os.chdir(script_dir)
-
-# Lazily load images
-bk = Image.open('./assets/bk.png')
-bq = Image.open('./assets/bq.png')
-bb = Image.open('./assets/bb.png')
-bn = Image.open('./assets/bn.png')
-br = Image.open('./assets/br.png')
-bp = Image.open('./assets/bp.png')
-
-wk = Image.open('./assets/wk.png')
-wq = Image.open('./assets/wq.png')
-wb = Image.open('./assets/wb.png')
-wn = Image.open('./assets/wn.png')
-wr = Image.open('./assets/wr.png')
-wp = Image.open('./assets/wp.png')
-
-os.chdir(cwd)
 
 # You can change the size of gif (BOARD_EDGE x BOARD_EDGE)
-# But it is not recommended unless you update piece images to fit squares
+# But it is not recommended unless piece images are updated to fit into squares
 BOARD_EDGE = 480
 SQUARE_EDGE = BOARD_EDGE // 8
 
@@ -47,84 +26,84 @@ def coordinates_of_square(square):
         return (c * SQUARE_EDGE, (7 - r) * SQUARE_EDGE)
 
 
-def clear(image, crd):
+def reset_square_color(image, square):
+    crd = coordinates_of_square(square)
+
     if (crd[0] + crd[1]) % (SQUARE_EDGE * 2) == 0:
         image.paste(white_square, crd, white_square)
     else:
         image.paste(black_square, crd, black_square)
 
 
-def apply_move(board_image, current, previous):
-    changed = [s for s in current.keys() if current[s] != previous[s]]
+def update_board_image(board_image, game_state, changed_squares):
+    for square in changed_squares:
+        reset_square_color(board_image, square)
+        piece = game_state[square]
 
-    for square in changed:
-        crd = coordinates_of_square(square)
-        clear(board_image, crd)
-
-        if current[square] != '':
-            exec('board_image.paste({0}, crd, {0})'.format(current[square]))
+        if piece:
+            paste_image_into_square(
+                board_image, piece, coordinates_of_square(square))
 
 
 def create_gif(pgn, gif_path, duration):
     board_image = initial_board.copy()
     images = [array(board_image)]
 
-    game = chess.ChessGame()
-    moves = chess.get_moves_from_pgn(pgn)
+    game = chess.ChessGame(pgn=pgn)
 
-    for move in moves:
+    while not game.is_finished:
         previous = game.state.copy()
-        game.push(move)
-        apply_move(board_image, game.state, previous)
+        game.next()
+        update_board_image(board_image, game.state, [
+                           s for s in game.state.keys() if game.state[s] != previous[s]])
         images.append(array(board_image))
 
-    last = images[len(moves)]
-    for i in range(3):
+    last = images[len(images) - 1]
+    for _ in range(3):
         images.append(last)
 
     imageio.mimsave(gif_path, images, duration=duration)
 
 
-def process_file(pgn, duration, output_dir):
-    name = os.path.basename(pgn)[:-4] + '.gif'
-    if name in os.listdir(output_dir):
-        print('gif with name %s already exists.' % name)
-    else:
-        gif_path = output_dir + '/' + name
-        print('Creating ' + name, end='... ')
-        create_gif(pgn, gif_path, duration)
-        print('done')
+def paste_image_into_square(image, piece, crd):
+    exec('image.paste({0}, {1}, {0})'.format(piece, crd))
+
+
+def load_assets():
+    cwd = os.getcwd()
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    os.chdir(script_dir)
+
+    # Lazily load images
+    bk = Image.open('./assets/bk.png')
+    bq = Image.open('./assets/bq.png')
+    bb = Image.open('./assets/bb.png')
+    bn = Image.open('./assets/bn.png')
+    br = Image.open('./assets/br.png')
+    bp = Image.open('./assets/bp.png')
+
+    wk = Image.open('./assets/wk.png')
+    wq = Image.open('./assets/wq.png')
+    wb = Image.open('./assets/wb.png')
+    wn = Image.open('./assets/wn.png')
+    wr = Image.open('./assets/wr.png')
+    wp = Image.open('./assets/wp.png')
+    globals().update(locals())
+
+    os.chdir(cwd)
 
 
 def generate_board():
     global initial_board
     initial_board = Image.new('RGB', (BOARD_EDGE, BOARD_EDGE))
-
-    for i in range(0, BOARD_EDGE, SQUARE_EDGE):
-        for j in range(0, BOARD_EDGE, SQUARE_EDGE):
-            clear(initial_board, (i, j))
-
-    row = ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r']
-    order = ('w', 'b') if reverse else ('b', 'w')
-
-    for i in range(8):
-        col = SQUARE_EDGE * i
-        exec('initial_board.paste({0}, (col, 0), {0})'
-            .format(order[0] + row[i]))
-        exec('initial_board.paste({0}, (col, BOARD_EDGE - SQUARE_EDGE), {0})'
-            .format(order[1] + row[i]))
-
-        exec('initial_board.paste({0}p, (col, SQUARE_EDGE), {0}p)'
-            .format(order[0]))
-        exec(
-            'initial_board.paste({0}p, (col, BOARD_EDGE - (SQUARE_EDGE * 2)), {0}p)'
-            .format(order[1]))
+    update_board_image(initial_board, chess.INITIAL_STATE,
+                       list(chess.INITIAL_STATE.keys()))
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        'path', nargs='*', help='Path to the pgn file/folder')
+        'path', nargs='*', help='Path to the pgn file')
     parser.add_argument(
         '-d', '--delay', help='Delay between moves in seconds', default=0.4)
     parser.add_argument(
@@ -142,7 +121,8 @@ def main():
     args = parser.parse_args()
 
     if not args.path:
-        print('Please specify path or directory of pgn files')
+        print('Please specify path of pgn file(s)')
+        return
 
     global reverse
     reverse = args.reverse
@@ -153,15 +133,21 @@ def main():
     black_square = Image.new('RGBA', (SQUARE_EDGE, SQUARE_EDGE),
                              args.black_square_color)
 
+    load_assets()
     generate_board()
 
-    for path in args.path:
-        if os.path.isfile(path):
-            process_file(path, args.delay, args.out)
-
-        elif os.path.isdir(path):
-            for pgn in glob.glob(path + '/*.pgn'):
-                process_file(pgn, args.delay, args.out)
+    for pgn in args.path:
+        try:
+            name = os.path.basename(pgn)[:-4] + '.gif'
+            if name in os.listdir(args.out):
+                print('A gif with name {0} already exists.'.format(name))
+            else:
+                out_path = os.path.join(args.out, name)
+                create_gif(pgn, out_path, args.delay)
+                print('Created {0}'.format(name))
+        except Exception as e:
+            print(e)
+            print("Error processing pgn file {0}".format(pgn))
 
 
 if __name__ == '__main__':
